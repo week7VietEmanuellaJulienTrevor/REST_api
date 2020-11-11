25880e3d733025d574067d69559d9669652a3fc9
trevorius <trevorius45@hotmail.com>
2020-11-11 11:47:41 -0500

    create leads model

---
+++ Intervention_management/Models/leads.cs
@@ -0,0 +1,19 @@
+using System;
+
+
+public class Leads
+{
+    public long Id { get; set; }
+    public string contact_full_name { get; set; }
+    public string company_name { get; set; }
+    public string email { get; set; }
+    public string phone { get; set; }
+    public string project_name { get; set; }
+    public string project_description { get; set; }
+    public string department { get; set; }
+    public string message { get; set; }
+    // public string attached_file { get; set; }
+    public DateTime created_at { get; set; }
+    public DateTime updated_at { get; set; }
+    public string attached_file_path { get; set; }
+}
